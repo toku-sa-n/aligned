9ce013331815403adafa7f603161c573707052f1
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-08 23:51:41 +0900

    docs(slice): add examples
    `try_from_raw_parts_mut`.

--- src/slice.rs
+++ src/slice.rs
@@ -40,6 +40,27 @@ pub unsafe fn from_raw_parts_mut<'a, T>(data: *mut T, len: usize) -> &'a mut [T]
 ///
 /// - [`Error::Null`] - `p` is null.
 /// - [`Error::NotAligned`] - `p` is not aligned correctly.
+///
+/// # Examples
+///
+/// ```rust
+/// use aligned::slice;
+/// use aligned::Error;
+///
+/// let mut x = 3;
+/// let s = unsafe { slice::try_from_raw_parts_mut(&mut x, 1) };
+///
+/// assert!(s.is_ok());
+/// assert_eq!(s.unwrap(), [3]);
+///
+/// let p: *mut i32 = core::ptr::null_mut();
+/// let s = unsafe { slice::try_from_raw_parts_mut(p, 1) };
+/// assert_eq!(s, Err(Error::Null));
+///
+/// let p = 0x1001 as *mut i32;
+/// let s = unsafe { slice::try_from_raw_parts_mut(p, 1) };
+/// assert_eq!(s, Err(Error::NotAligned));
+/// ```
 pub unsafe fn try_from_raw_parts_mut<'a, T>(
     data: *mut T,
     len: usize,
