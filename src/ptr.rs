af8c4aa839372807fe5805ae46ac365a76151f42
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-09 13:37:01 +0900

    feat(ptr): implement `try_copy`

--- src/ptr.rs
+++ src/ptr.rs
@@ -345,3 +345,52 @@ pub unsafe fn try_write<T>(p: *mut T, v: T) -> Result<(), Error> {
     unsafe { p.write(v) };
     Ok(())
 }
+
+/// The wrapper of [`core::ptr::copy`] which may return an error unless the passed pointers are
+/// aligned and not null.
+///
+/// # Safety
+///
+/// The caller must follow the safety rules required by [`core::ptr::copy`] except the alignment
+/// and null rules.
+///
+/// # Errors
+///
+/// This method may return an error:
+///
+/// - [`Error::Null`] - Either `src` or `dst` is null.
+/// - [`Error::NotAligned`] - Either `src` or `dst` is not aligned correctly.
+///
+/// # Examples
+///
+/// ```rust
+/// use aligned::ptr;
+/// use aligned::Error;
+/// use core::mem::MaybeUninit;
+///
+/// let x = 3;
+/// let src = &x as *const i32;
+/// let mut y = MaybeUninit::uninit();
+/// let dst = y.as_mut_ptr();
+///
+/// let r = unsafe { ptr::try_copy(src, dst, 1) };
+/// assert!(r.is_ok());
+/// let y = unsafe { y.assume_init() };
+/// assert_eq!(y, 3);
+///
+/// let dst = core::ptr::null_mut();
+/// let r = unsafe { ptr::try_copy(src, dst, 1) };
+/// assert_eq!(r, Err(Error::Null));
+///
+/// let dst = 0x1001 as *mut i32;
+/// let r = unsafe { ptr::try_copy(src, dst, 1) };
+/// assert_eq!(r, Err(Error::NotAligned));
+/// ```
+pub unsafe fn try_copy<T>(src: *const T, dst: *mut T, count: usize) -> Result<(), Error> {
+    return_error_on_null_or_misaligned(src)?;
+    return_error_on_null_or_misaligned(dst)?;
+
+    // SAFETY: The caller must uphold the all safety rules.
+    unsafe { core::ptr::copy(src, dst, count) };
+    Ok(())
+}
