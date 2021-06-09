5211a3e1a6540dd0a232684d34c3bf068619d464
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-09 17:53:12 +0900

    feat(ptr): implement `write_bytes` (#22)

--- src/ptr.rs
+++ src/ptr.rs
@@ -408,6 +408,74 @@ pub unsafe fn try_write<T>(p: *mut T, v: T) -> Result<()> {
     Ok(())
 }
 
+/// The wrapper of [`core::ptr::write_bytes`] which panics if the passed pointer is null or not
+/// aligned.
+///
+/// # Safety
+///
+/// The caller must follow the safety rules required by [`core::ptr::write_bytes`] except the
+/// alignment and null rules.
+///
+/// # Examples
+///
+/// ```rust
+/// use aligned::ptr;
+///
+/// let mut slice = [0_u8; 4];
+/// unsafe { ptr::write_bytes(slice.as_mut_ptr(), 0xff, 4) };
+///
+/// assert_eq!(slice, [0xff, 0xff, 0xff, 0xff]);
+/// ```
+pub unsafe fn write_bytes<T>(dst: *mut T, val: u8, count: usize) {
+    // SAFETY: The caller must uphold the safety requirements.
+    unsafe { try_write_bytes(dst, val, count).expect(ERR_MSG) }
+}
+
+/// The wrapper of [`core::ptr::write_bytes`] which returns an error if the passed pointer is null or not
+/// aligned.
+///
+/// # Safety
+///
+/// The caller must follow the safety rules required by [`core::ptr::write_bytes`] except the
+/// alignment and null rules.
+///
+/// # Errors
+///
+/// This function returns an error:
+///
+/// - [`crate::Error::Null`] - `dst` is null.
+/// - [`crate::Error::NotAligned`] - `dst` is not aligned.
+///
+/// # Examples
+///
+/// ```rust
+/// use aligned::ptr;
+/// use aligned::Error;
+///
+/// let mut slice = [0_u8; 4];
+///
+/// let r = unsafe { ptr::try_write_bytes(slice.as_mut_ptr(), 0xff, 4) };
+/// assert!(r.is_ok());
+/// assert_eq!(slice, [0xff, 0xff, 0xff, 0xff]);
+///
+/// let p: *mut i32 = core::ptr::null_mut();
+/// let r = unsafe { ptr::try_write_bytes(p, 0xff, 4) };
+/// assert_eq!(r, Err(Error::Null));
+///
+/// let p = 0x1001 as *mut i32;
+/// let r = unsafe { ptr::try_write_bytes(p, 0xff, 4) };
+/// assert_eq!(r, Err(Error::NotAligned));
+/// ```
+#[allow(clippy::shadow_unrelated)]
+pub unsafe fn try_write_bytes<T>(dst: *mut T, val: u8, count: usize) -> Result<()> {
+    return_error_on_null_or_misaligned(dst)?;
+
+    // SAFETY: The caller must uphold the safety requirements.
+    unsafe { core::ptr::write_bytes(dst, val, count) };
+
+    Ok(())
+}
+
 /// The wrapper of [`core::ptr::copy`] which panics unless the passed pointers are aligned and not
 /// null.
 ///
